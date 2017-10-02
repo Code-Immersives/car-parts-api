54aaab832b064e3dd87bd3b9daafd4e2b4c9c22f
toneloke <guerrerotonyjs@gmail.com>
2017-10-02 16:36:38 -0400

    route file has id or make

---
+++ cars.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    "make": "ford",
+    "model": "explorer",
+    "year": 2005,
+    "id": 17
+  },
+  {
+    "make": "honda",
+    "model": "civic",
+    "year": 2017,
+    "id": 11
+  },
+  {
+    "make": "bmw",
+    "model": "328i",
+    "year": 1997,
+    "id": 1
+  }
+]
